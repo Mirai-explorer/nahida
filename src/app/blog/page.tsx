d081b8ed5bff74dab3d7b24063172d9b2f9c2c79
Mirai-explorer <tangjw0226@gmail.com>
2023-09-17 21:28:43 +0800

    File updates

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,10 +3,9 @@ import List from '@/components/Blog/List';
 import Card from '@/components/Blog/Card';
 import articles from '@/public/data/articles';
 import PictureViewer from "@/components/Blog/PictureViewer";
-import React, {Suspense} from 'react';
+import React from 'react';
 import { useEventEmitter } from 'ahooks';
 import { EventEmitter } from "ahooks/lib/useEventEmitter";
-import Loading from "@/app/blog/loading";
 
 const Blog = () => {
     const event$: EventEmitter<any> = useEventEmitter();
@@ -14,9 +13,7 @@ const Blog = () => {
         <>
             <List>
                 {articles.map((article) => (
-                    <Suspense fallback={<Loading count={articles.length} />}>
-                        <Card key={article.post.post_id} article={article} event$={event$} />
-                    </Suspense>
+                    <Card key={article.post.post_id} article={article} event$={event$} />
                 ))}
             </List>
             <PictureViewer event$={event$}></PictureViewer>
